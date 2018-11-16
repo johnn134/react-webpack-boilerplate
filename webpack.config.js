996ba0f47136982a84fb8cfcc7e466f6cca59bb2
John Nelson <johnn134@gmail.com>
2018-11-15 19:52:48 -0500

    Upgrade to Webpack 4 and Babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,40 @@
-const webpack = require('webpack');
-const path = require('path');
-const fs = require('fs');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const ROOT_PATH = path.resolve(__dirname);
-const ENTRY_PATH = path.resolve(ROOT_PATH, 'src/js/index');
-const TEMPLATE_PATH = path.resolve(ROOT_PATH, 'src/index.html');
-const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
-
-const PORT = 8080;
-
-const plugins = [
-    new HtmlWebpackPlugin({
-        inject: 'body',
-        template: TEMPLATE_PATH
-    }),
-    new ExtractTextPlugin('css/styles.css'),
-    new OpenBrowserPlugin({
-        url: `http://localhost:${PORT}/`
-    }),
-    new webpack.HotModuleReplacementPlugin()
-];
-
-if (fs.existsSync('./src/images')) {
-
-    plugins.push(
-        new CopyWebpackPlugin([
-            {
-                from: './src/images',
-                to: './images'
-            }
-        ]),
-    );
-
-}
-
-var config = {
-    entry: ENTRY_PATH,
-    devtool: 'source-map',
-    plugins: plugins,
-    output: {
-        path: BUILD_PATH,
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+module.exports = {
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['babel-preset-env', 'babel-preset-react', 'babel-preset-stage-2']
-                }
+                use: [
+                    "babel-loader"
+                ]
             },
             {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+                test: /\.html$/,
+                use: [
+                    "html-loader"
+                ]
             },
             {
-                test: /\.css?$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                })
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             },
             {
-                test: /\.png$/,
-                exclude: /node_modules/,
-                loader: 'file-loader?name=images/[name].[ext]'
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
 };
-
-module.exports = config;
